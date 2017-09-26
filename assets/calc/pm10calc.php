8350d1551b665561d96a6a2b330e0014a1f2cbdb
Adam Paszternak <hello@paszternak.me>
2017-09-26 12:37:21 +0200

    Create pm10calc.php

---
+++ assets/calc/pm10calc.php
@@ -0,0 +1,13 @@
+<?php 
+if ($pm10v < 20) {
+	$pm10code = 'elfogadható';
+} else if ($pm10v >= 20 && $pm10v < 40) {
+	$pm10code = 'tűrhető';
+} else if ($pm10v >= 40 && $pm10v < 50) {
+	$pm10code = 'problémás';
+} else if ($pm10v >= 50 && $pm10v < 100) {
+	$pm10code = 'egészségtelen';
+} else if ($pm10v >= 100) {
+	$pm10code = 'veszélyes';
+}
+?>
