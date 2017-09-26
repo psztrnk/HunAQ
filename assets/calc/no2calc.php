2f0dba2c12995045a9d9c0843d578717dc60595d
Adam Paszternak <hello@paszternak.me>
2017-09-26 12:36:22 +0200

    Create no2calc.php

---
+++ assets/calc/no2calc.php
@@ -0,0 +1,13 @@
+<?php 
+if ($no2v < 16) {
+	$no2code = 'elfogadható';
+} else if ($no2v >= 16 && $no2v < 32) {
+	$no2code = 'tűrhető';
+} else if ($no2v >= 32 && $no2v < 40) {
+	$no2code = 'problémás';
+} else if ($no2v >= 40 && $no2v < 80) {
+	$no2code = 'egészségtelen';
+} else if ($no2v >= 80) {
+	$no2code = 'veszélyes';
+}
+?>
