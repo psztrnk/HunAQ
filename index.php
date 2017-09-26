97b6c3c39b33febab70b495736632c253a8e6f43
Adam Paszternak <hello@paszternak.me>
2017-09-26 13:04:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!doctype HTML>
 <html>
 	<head>
+		<meta charset="utf-8">
+    		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="assets/css/style.css" type="text/css" />
 		<title>Levegőminőség</title>
 	</head>
